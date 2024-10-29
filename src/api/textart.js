6df6aed2c3eb132a0acc7c374f79c7af611295d7
SotoEstévez <ricardo@sotoestevez.dev>
2024-10-29 14:22:28 +0100

    fix: ascii api

--- src/api/textart.js
+++ src/api/textart.js
@@ -1,11 +1,11 @@
 /**
- * Reach out to the textart API and get the ascii art with the text.
+ * Reach out to the asciified API and get the ascii art with the text.
+ * This function used to query the textart APIO but it's not longer working,
+ * but we'll the name of the package to evade spreading changes.
+ *
  * @returns The decoded ASCII text.
  */
 export const getAscii = async (text) => {
-  const response = await fetch(
-    `http://api.textart.io/figlet.json?text=Puto ${text}&encode=false`
-  );
-  const data = await response.json();
-  return data.contents?.figlet;
+  return fetch(`https://asciified.thelicato.io/api/v2/ascii?text=Puto ${text}`)
+    .then(res => res.text());
 };
